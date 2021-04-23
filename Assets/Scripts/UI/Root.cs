bcbf7d677d1bd147e5844f6752173ebf41ebd841
89trillion-Zhaochi <zhaochi@89trillion.com>
2021-04-23 14:28:02 +0800

    Init

---
+++ Assets/Scripts/UI/Root.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Root : MonoBehaviour
+{
+    //点击Root按钮事件
+    public void OpenButtonOnClick()
+    {
+        Debug.Log("OpenButtonOnClick");
+    }
+}
