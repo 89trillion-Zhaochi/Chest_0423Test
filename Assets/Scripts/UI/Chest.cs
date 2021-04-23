2791cc2a0ce9081f717c89e39d2ca47510574705
89trillion-Zhaochi <zhaochi@89trillion.com>
2021-04-23 18:05:00 +0800

    导入了三个插件
    unity 动画系统
    Dotween 动画
    AE 动画
    Spine动画

---
+++ Assets/Scripts/UI/Chest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] private Transform chestTs;
+
+    public void CloseButtonOnClick()//销毁chest
+    {
+        Destroy(chestTs.gameObject);
+    }
+}
--- Assets/Scripts/UI/Root.cs
+++ Assets/Scripts/UI/Root.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Root : MonoBehaviour
 {
+    [SerializeField] private Transform rootTs;
+
+    [SerializeField] private GameObject chest;
     //点击Root按钮事件
     public void OpenButtonOnClick()
     {
+        var _chest = Instantiate(chest, rootTs);
+        _chest.name = "Chest";
+        _chest.transform.localScale = Vector3.one;
+        _chest.transform.localPosition = Vector3.zero;
         Debug.Log("OpenButtonOnClick");
     }
 }
